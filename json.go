8117416422e79e6828502cca3b22724575f9ce57
Mustafa Ozturk <mozturkpro@gmail.com>
2024-12-26 19:38:03 -0500

    fix helper func

--- json.go
+++ json.go
@@ -26,11 +26,11 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		w.WriteHeader(500)
 		return
 	}
+	w.WriteHeader(code)
 	_, err = w.Write(dat)
 	if err != nil {
 		log.Printf("Error writing data: %s", err)
 		w.WriteHeader(500)
 		return
 	}
-	w.WriteHeader(code)
 }
